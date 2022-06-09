5e3a8de045ea9fab174e02775eb32fc577059a23
FatjonMileti <tonimileti@yahoo.com>
2022-06-09 06:53:01 -0700

    task 13

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n, s;
+
+	for (n = 1; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			s = s + n;
+		}
+	}
+	printf("%i\n", s);
+}
