e3126e822b9c0f1bc376c89046d233de8422e2f2
FatjonMileti <tonimileti@yahoo.com>
2022-06-10 11:41:21 -0700

    task 15

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - print sum of even Fionacci sequence up to 4,000,000
+ * main -- entry point
  * Return: 0
  */
-
 int main(void)
 {
-	int sum_ev = 0;
-	int pri;
-	int sec;
-	int sum = 1;
+	long pri = 1, sec = 2, x;
+	long sum = 0;
 
-	pri = 1;
-	sec = 1;
-
-	while (sec < 4000000)
+	for (x = 0; x < 4000000; x++)
 	{
-		sum = pri + sec;
+		x = pri + sec;
 		pri = sec;
-		sec = sum;
-		if ((sum <= 4000000) && (sum % 2 == 0))
-			sum_ev += sum;
+		sec = x;
+		if (pri % 2 == 0)
+		{
+			sum += pri;
+		}
 	}
-	printf("%d\n", sum_ev);
-
+	printf("%li\n", sum);
 	return (0);
 }
