f026814f69419f320ad0f805154486d69967483a
FatjonMileti <tonimileti@yahoo.com>
2022-06-13 03:22:14 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_line - check is digit
+ *@n: intiger
+ *
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
