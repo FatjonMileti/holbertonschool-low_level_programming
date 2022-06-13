e433c2dbbdcfda663b252383734a02a64b8d6315
FatjonMileti <tonimileti@yahoo.com>
2022-06-13 11:23:03 -0700

    task 12

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_number - check is digit
+ *@n:integer
+ *
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+void print_number(int n)
+{
+	unsignent int x;
+
+	if(n<0)
+	{
+		_putchar('-');
+		x = -n;
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+{
+	print_number(x / 10);
+}
+	_putchar((x % 10) + '0');
+}
+
