82c3e8e15ebd9961dbdcfe112c8855e6f4c7cad3
FatjonMileti <tonimileti@yahoo.com>
2022-06-13 06:18:46 -0700

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@
 void print_square(int size)
 {
 	int r, c;
-	
+
 	for (r = 0; r < size; r++)
 	{
 		for (c = 0; c < size; c++)
@@ -18,5 +18,5 @@ void print_square(int size)
 		_putchar('\n');
 	}
 	if (size <= 0)
-		_putchar('\n');	
+		_putchar('\n');
 }
