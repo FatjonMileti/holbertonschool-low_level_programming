9b54d071a1e20ef619dc01aba79036bfe0ec05da
FatjonMileti <tonimileti@yahoo.com>
2022-06-13 05:24:08 -0700

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code.
+ * @n: to check
+ * Return:  0 or 1.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		i{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (i < 1)
+		_putchar('\n');
+}
