b3d5ac2c9c61e07609c3eafba4092a70e414a80d
FatjonMileti <tonimileti@yahoo.com>
2022-06-13 02:09:23 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - check is digit
+ *
+ *
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
