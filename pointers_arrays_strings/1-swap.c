5b583d15f90325a9a49d904dda349fc7e0750cb9
FatjonMileti <tonimileti@yahoo.com>
2022-06-14 10:34:05 -0700

    task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *reset_to_98 - updates to 98
- *@n:takes an integer
+ *swap_int - updates to 98
+ *@a:takes an integer
+ *@b:takes an integer
  */
 void swap_int(int *a, int *b)
 {
