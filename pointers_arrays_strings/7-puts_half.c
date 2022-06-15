8deb2b651d912ddc017df535cfa7eee4736cf9e3
FatjonMileti <tonimileti@yahoo.com>
2022-06-15 06:40:47 -0700

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,16 +5,21 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int i = 0;
+	int len = 0;
+	int n;
 
-	i = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (i = i / 2; str[i] != '\0'; i++)
-	{
-	_putchar (str[i]);
-	}
+	while (str[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
