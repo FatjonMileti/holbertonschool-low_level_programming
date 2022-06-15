e57ad6236188e4cc0f02d919bde41b28305143c2
FatjonMileti <tonimileti@yahoo.com>
2022-06-15 07:16:43 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - updates to 98
+ *@a:takes an integer
+ *@n:takes an integer
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d \n", a[i]);
+	}
+}
