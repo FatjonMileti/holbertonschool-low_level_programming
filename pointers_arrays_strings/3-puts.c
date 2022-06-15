ed48229239165426902ef469fd97f8a6a962b832
FatjonMileti <tonimileti@yahoo.com>
2022-06-15 00:59:47 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_puts - updates to 98
+ *@str:takes an integer
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
