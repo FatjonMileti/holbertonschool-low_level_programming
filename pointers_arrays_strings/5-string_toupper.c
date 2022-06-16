858c4687bd4d9e1507d837a1a9c2b76e7766d7f6
FatjonMileti <tonimileti@yahoo.com>
2022-06-16 04:15:54 -0700

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - concatenate two strings
+ * @s: string
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
