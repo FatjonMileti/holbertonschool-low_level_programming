4517a245960fc890ada18ad1b6aa1f24c39ca2be
FatjonMileti <tonimileti@yahoo.com>
2022-06-16 02:28:31 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
