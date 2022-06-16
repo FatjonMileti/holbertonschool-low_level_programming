2f7d9db33e3832ea10b948811168964f0222f669
FatjonMileti <tonimileti@yahoo.com>
2022-06-16 03:49:04 -0700

    task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - concatenate two strings
+ * @n: string
+ * @a: string
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
