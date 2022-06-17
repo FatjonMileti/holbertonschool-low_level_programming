61c96811d44471021dcf71fed84fcc8f595dc97b
FatjonMileti <tonimileti@yahoo.com>
2022-06-17 04:19:41 -0700

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strspn - function
+ *@s: char
+ *@accept: char
+ *Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int j = 0;
+	int i, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+		if (s[i] == accept[k])
+			j++;
+		break;
+		}
+	}
+	if (accept[k + 1] == 0)
+		return (j);
+	return (j);
+}
