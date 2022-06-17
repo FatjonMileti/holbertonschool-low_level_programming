962efe253a5a64343add68b4ca78f25006aa05ab
FatjonMileti <tonimileti@yahoo.com>
2022-06-17 04:33:36 -0700

    task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- *_strspn - function
- *@s: char
- *@accept: char
- *Return: s
+ * _strspn - recreate memeset
+ * @s: a pointer in memory file
+ * @accept: the value to be set
+ * Return: s
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int j = 0;
 	int i, k;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (k = 0; s[k] != '\0'; k++)
 	{
-		for (k = 0; accept[k] != '\0'; k++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
-			if (s[i] == accept[k])
+			if (s[k] == accept[i])
 			{
 				j++;
 				break;
 			}
+			if (accept[i + 1] == '\0')
+				return (j);
 		}
 	}
-	if (accept[k + 1] == 0)
-		return (j);
 	return (j);
 }
