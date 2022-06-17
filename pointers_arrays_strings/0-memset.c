7c040676367255fc2a306048371207a80bcfdffd
FatjonMileti <tonimileti@yahoo.com>
2022-06-17 02:53:24 -0700

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_memset - function
+ *@s: char
+ *@b: char
+ *@n: intiger
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
