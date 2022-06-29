2e38964f350fc1a901e5c46391fddb3ec23e57c1
FatjonMileti <tonimileti@yahoo.com>
2022-06-29 03:13:06 -0700

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *print_dog - new dog
+ *@d: structure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1;
+	int i, nlen = 0, olen = 0;
+
+	while (name[nlen])
+		nlen++;
+	while (owner[olen])
+		olen++;
+	dog1 = malloc (sizeof(dog_t));
+	if (dog1 == NULL)
+		return (NULL);
+	dog1->name = malloc (sizeof(char) * (nlen + 1));
+	if (dog1->name == NULL)
+	{
+		free(dog1);
+		return (NULL);
+	}
+	dog1->owner = malloc (sizeof(char) * (olen + 1));
+	if (dog1->owner == NULL)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	dog1->age = age;
+	for (i = 0; name[i]; i++)
+		dog1->name[i] = name[i];
+	for (i = 0; owner[i]; i++)
+		dog1->owner[i] = owner[i];
+	dog1->owner[i] = '\n';
+	return (dog1);
+}
