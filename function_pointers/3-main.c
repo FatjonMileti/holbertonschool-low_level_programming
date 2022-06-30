21f2be4ff72522992ccd39943448066dc1f6c4ed
FatjonMileti <tonimileti@yahoo.com>
2022-06-30 04:04:39 -0700

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,34 +1,37 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
+
+
 /**
- *
+ * main - calculator
+ * @argv: argv
+ * @argc: argc
+ * Return: 0;
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int num1, num2;
-	char *f;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	f = argv[2];
 	num2 = atoi(argv[3]);
-
-	if ((*f == '/' ||  *f == '%') && num2 == 0)
-
+	op = argv[2];
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit (100);
+		exit(99);
 	}
-	if (get_op_func(f) == NULL || f[1] != '\0')
+	if ((*op == '/' || *op == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(100);
 	}
-        printf ("%d\n", get_op_func(f)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
