b065979a10abf6d26de87373325011e0f9376363
FatjonMileti <tonimileti@yahoo.com>
2022-07-14 01:53:27 -0700

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
 
 	while (tmp)
 	{
-		tmp = tmp->next;
+		tmp->next = new;
+		new->next = NULL;
 
 		if (i == idx)
 		{
