05798afce7717a553e35a6c7d72da924a2e0f9c3
FatjonMileti <tonimileti@yahoo.com>
2022-07-14 02:13:33 -0700

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,20 +1,23 @@
 #include "lists.h"
-/**
- *
- */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: head
+ * @idx: idx
+ * @n: n
+ * Return: new
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
 	unsigned int i = 0;
 	listint_t *new, *tmp;
 
-	 if (!*head)
-                return (NULL);
-
-
-	new = malloc (sizeof(listint_t));
-	if (!new)
+	if (head == NULL)
 		return (NULL);
-
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 
 	if (idx == 0)
@@ -23,21 +26,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
 		*head = new;
 		return (new);
 	}
-
 	tmp = *head;
-
-	while (tmp)
+	while (i < (idx - 1))
 	{
-
-		if (i == idx)
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
 		tmp = tmp->next;
 		i++;
+
+		if (tmp == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
 	}
+
 	new->next = tmp->next;
 	tmp->next = new;
 	return (new);
