a06df03ae1819a64eba365b217ea519b0e76841e
FatjonMileti <tonimileti@yahoo.com>
2022-07-14 01:36:20 -0700

    task 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
+	unsigned int i;
+	listint_t *new, *tmp;
+
+	 if (!*head)
+                return (NULL);
+
+
+	new = malloc (sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	tmp = *head;
+
+	while (tmp)
+	{
+		if (i == idx)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (new);
+}
