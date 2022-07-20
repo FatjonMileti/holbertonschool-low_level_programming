f042703444556874fd4a1ea59daa81eeb472be42
FatjonMileti <tonimileti@yahoo.com>
2022-07-20 08:07:20 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,68 +1,48 @@
 #include "main.h"
-/**
- * main - check the code
- *@argv: ...
- *@argv: ...
- * Return: Always 0.
- */
 
-int main(int argc, char **argv)
+/**
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
+ */
+int main(int argc, char *argv[])
 {
-	int fd, fd1, wf, rf, cl;
-	char *txt[1024];
+	int fd, fd1, rf, cl, cl1;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(argv[1], O_RDWR);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd1 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (fd1 == -1)
+	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((rf = read(fd, buf, BUFSIZ)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (fd1 == -1 || write(fd1, buf, rf) != rf)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd);
+			exit(99);
+		}
 	}
-
-	rf = read(fd, txt, 1024);
 	if (rf == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	while (rf)
-	{
-		wf = write(fd1, txt, 1024);
-		if (wf == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		rf = read(fd, txt, 1024);
-		if (rf == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-	}
 	cl = close(fd);
-	if (cl == -1)
+	cl1 = close(fd1);
+	if (cl == -1 || cl1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit (100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(100);
 	}
-
-	cl = close(fd1);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-
 	return (0);
 }
