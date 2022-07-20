8928606eda00ddefb10e3c735f429c80d8ee407a
FatjonMileti <tonimileti@yahoo.com>
2022-07-20 08:14:48 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int fd, fd1, rf, cl, cl1;
-	char buf[BUFSIZ];
+	char txt[1024];
 
 	if (argc != 3)
 	{
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((rf = read(fd, buf, BUFSIZ)) > 0)
+	while ((rf = read(fd, txt, txtSIZ)) > 0)
 	{
-		if (fd1 == -1 || write(fd1, buf, rf) != rf)
+		if (fd1 == -1 || write(fd1, txt, rf) != rf)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd);
@@ -39,10 +39,13 @@ int main(int argc, char *argv[])
 	}
 	cl = close(fd);
 	cl1 = close(fd1);
-	if (cl == -1 || cl1 == -1)
+	if (cl == -1)
 	{
+		if (cl1 == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 			exit(100);
+		}
 	}
 	return (0);
 }
