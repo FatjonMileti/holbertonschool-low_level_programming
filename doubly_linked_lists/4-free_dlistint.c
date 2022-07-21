436dfc9b46e5f35109198699db1cba32bfc09fd6
FatjonMileti <tonimileti@yahoo.com>
2022-07-21 01:30:53 -0700

    ...

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - ...
+ * @head: ...
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
