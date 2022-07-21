574b69d425a4700a95c781b9f39a2e785570b7fb
FatjonMileti <tonimileti@yahoo.com>
2022-07-20 21:10:13 -0700

    ...

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - ...
+ * @h: ...
+ * Return: ...
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		c++;
+	}
+
+	return (c);
+}
