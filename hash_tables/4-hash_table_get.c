166c987937eace4a3586ed6e7be543561f007b4d
FatjonMileti <tonimileti@yahoo.com>
2022-08-18 05:54:41 -0700

    task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - ...
+ *@ht: ...
+ *@key: ...
+ *Return: ...
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *new, *current;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[idx];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
