8dc817d491340de1fb6dbd9a55ebe9de9cd90ceb
FatjonMileti <tonimileti@yahoo.com>
2022-08-19 01:01:35 -0700

    test

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *current;
+	short flag = 0;
+	
+	if (ht == NULL)
+		return;
+	printf("{");
+	i = 0;
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf (", ");
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				printf("'%s, %s'", current->key, current->value);
+				current = current->next;
+				if (current != NULL)
+					printf(", ");
+			}
+		}
+			flag = 1;
+	}
+		i++;
+
+	printf("{\n");
+}
