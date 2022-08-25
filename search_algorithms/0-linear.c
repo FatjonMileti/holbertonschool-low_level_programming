5996a2e8526c7a31a2734c029088a343a8f12fa7
FatjonMileti <tonimileti@yahoo.com>
2022-08-25 01:38:42 -0700

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,19 +3,23 @@
  * linear_search - ...
  * @array: ...
  * @size: ...
- * @valoe: ...
+ * @value: ...
+ * Return: ...
  */
 int linear_search(int *array, size_t size, int value)
 {
-	long unsigned int i;
+	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array)
 	{
+		for (i = 0; i < size; i++)
+		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-
 				return (i);
+		}
 	}
-		return (-1);
+	return (-1);
+
 
 }
