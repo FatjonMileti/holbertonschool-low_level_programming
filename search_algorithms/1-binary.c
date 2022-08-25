cb7ada1d7f504b47f2ee07eafed42e6ec9ebbe00
FatjonMileti <tonimileti@yahoo.com>
2022-08-25 03:01:07 -0700

    task 1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+ * binary_search - ...
+ * @array: ...
+ * @size: ...
+ * @value: ...
+ * Return: ...
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left, right, mid;
+
+	if (array)
+	{
+		while (left <= righ)
+		{
+			mid = (right + left) / 2;
+
+			printf("Searching in array: ");
+			for (i = left; i <= right; i++)
+			{
+				printf("%d", array[i]);
+				if (i != right)
+					printf(", ");
+			}
+			printf("\n");
+			if (array[mid] == value)
+				return (mid);
+			else if (array[mid] > value)
+				right = mid - 1;
+			else
+				left = mid + 1;
+		}
+	}
+	return (-1);
+}
