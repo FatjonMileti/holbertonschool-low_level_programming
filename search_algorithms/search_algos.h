af6b072cc3695d9a4ec7a59f394130ae26a2375b
FatjonMileti <tonimileti@yahoo.com>
2022-08-29 02:57:09 -0700

    ...

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,9 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int bin_search(int *array, size_t size, int value, int low, int high);
+int jump_search(int *array, size_t size, int value);
 
 #endif
