a6008ff0b20e9d376a327488459096bdf3f01e8d
FatjonMileti <tonimileti@yahoo.com>
2022-08-29 02:56:10 -0700

    ...

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+/**
+ * jump_search - ...
+ * @array: ...
+ * @size: ...
+ * @value: ...
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, start, end;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	start = 0;
+	end = 0;
+
+	while (array[end] < value && end < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", end, array[end]);
+		start = end;
+		end = end + sqrt(size);
+
+		if (start >= size)
+			return (-1);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+
+	end = MIN(end, size - 1);
+
+	for (i = start; i <= end; ++i)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
